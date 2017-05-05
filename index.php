8a607e31d27253bf2ac80a45528b9bf9f8166d63
Anton Andersen <a.andersen@syncleo-iot.com>
2017-05-05 23:11:23 +0000

    Force PHP to die after an error of any type. Print nice error messages with traces

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 #!/usr/bin/env php
 <?php
 
+/**
+ * Do some preparations to have nice errors in CLI
+ * @see https://xdebug.org/docs/all_settings#default_enable
+ */
+ini_set('display_errors', 0);
+if (extension_loaded('xdebug')) {
+    xdebug_disable(); //Doesn't actually disable xdebug but prevent if from showing stack traces
+}
+
+function convertErrors($errno , $errstr, $errfile, $errline) {
+    print "ERROR: $errstr in $errfile on line $errline\n\n";
+    debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    trigger_error('Trigger error', E_USER_ERROR);
+}
+
+set_error_handler('convertErrors');
+
 require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/core/autoload.php');
 
